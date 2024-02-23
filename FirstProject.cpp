beb2364d3f339aac49b8f79a76e0e50435c418d5
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-02-23 16:47:36 +0200

    Update FirstProject.cpp

--- FirstProject.cpp
+++ FirstProject.cpp
@@ -2,14 +2,12 @@
 #include <fstream>
 #include <sstream>
 #include <limits>
-#include <climits>
 #include <chrono> 
 #include <cmath>
 #include <numeric>
 #include <iomanip>
 #include <algorithm>
 #include <random>
-#include <string>
 #include <vector>
 
 std::random_device rd;
@@ -48,7 +46,7 @@ void Netinkamas_String(std::string& ivestis)
 {
 	std::cin.clear();
 	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	std::cout << "Ivesti netinkami duomenys (Negali buti skaiciu ar specialiuju zenklu).Bandykite dar karta: ";
+	std::cout << "Ivesti netinkami duomenys. Bandykite dar karta: ";
 	std::cin >> ivestis;
 }
 
@@ -62,7 +60,7 @@ void Netinkamas_Int(int& ivestis)
 
 void Ivesti_Pazymius(studentas& S)
 {
-	std::cout << std::endl << "Iveskite namu darbu pazymi: ";
+	std::cout << "\nIveskite namu darbu pazymi: ";
 	int pazymys;
 	do
 	{
@@ -82,20 +80,13 @@ void Ivesti_Pazymius(studentas& S)
 	} while (TaipNePaz == 'T');
 }
 
-void GeneruotiNDPazymius(studentas& S)
+void GeneruotiNDPazymius(studentas& S, int ND_kiekis)
 {
-	do
+	for(int j = 0; j < ND_kiekis; j++)
 	{
 		int pazymys = dis(generuoti); // generuojmas random pazymys
 		S.ND.push_back(pazymys); // pridedamas pazymis i vektoriu
-
-		std::cout << std::endl << "Sugeneruotas pazymys: " << pazymys << std::endl;
-		std::cout << "Ar norite sugeneruoti dar viena pazymi? (iveskite T, jei taip , N, jei ne): ";
-		std::cin >> TaipNePaz;
-		while (TaipNePaz != 'T' && TaipNePaz != 'N')//tikrinama ar ivestas tinkamas simbolis
-			Netinkamas_Char(TaipNePaz);
-
-	} while (TaipNePaz == 'T');
+	}
 }
 
 void Ivesti_Varda(studentas& S)
@@ -167,34 +158,17 @@ std::vector<studentas> Nuskaityti_Is_Failo(const std::string& Failo_Pavadinimas)
 			student.EGZ = student.ND.back();
 			student.ND.pop_back();
 		}
-		else
-		{
-			std::string nextLine;
-			if (std::getline(file, nextLine))
-			{
-				std::istringstream nextIss(nextLine);
-				if (!(nextIss >> student.EGZ))
-				{
-					std::cerr << "Klaida nuskaitant egzamino bala nuo failo" << std::endl;
-					break;
-				}
-			}
-			else
-			{
-				std::cerr << "Netiketa failo pabaiga" << std::endl;
-				break;
-			}
-		}
+		
 
 		S.push_back(student); // Pridedamas studentas 
 	}
 	// Baigia skaiciuoti laika
 	auto end = std::chrono::high_resolution_clock::now();
 
-	//Apskaicciuoja laika
+	//Apskaiciuoja laika
 	auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
 
-	std::cout << "Failo nuskaitymas uztruko " << duration.count() << " sekundes(ziu)" << std::endl;
+	std::cout << "Failo nuskaitymas uztruko " << duration.count() << " sekundes(e)" << std::endl;
 	file.close();
 	return S;
 }
@@ -234,27 +208,22 @@ void Apskaiciuoti_Rezultatus(std::vector<studentas>& S)
 	}
 }
 
-bool compareFirstName(const studentas& a, const studentas& b) 
+bool VarduRusiavimas(const studentas& a, const studentas& b) 
 {
 	return a.vardas < b.vardas;
 }
 
-bool compareLastName(const studentas& a, const studentas& b) 
+bool PavardziuRusiavimas(const studentas& a, const studentas& b) 
 {
 	return a.pavarde < b.pavarde;
 }
 
-bool compareExamGrade(const studentas& a, const studentas& b) 
-{
-	return a.EGZ < b.EGZ;
-}
-
-bool compareFinalGradeMedian(const studentas& a, const studentas& b) 
+bool MedianuRusiavimas(const studentas& a, const studentas& b) 
 {
 	return a.GalutinisM < b.GalutinisM;
 }
 
-bool compareFinalGradeAverage(const studentas& a, const studentas& b) 
+bool VidurkiuRusiavimas(const studentas& a, const studentas& b)
 {
 	return a.GalutinisV < b.GalutinisV;
 }
@@ -262,28 +231,29 @@ bool compareFinalGradeAverage(const studentas& a, const studentas& b)
 void Rusiuoti_Duomenis(std::vector<studentas>& S)
 {
 	// Rusiavimo pasirinkimai
-	std::cout << std::endl << "Rusiuoti pagal:\n 1. Varda\n 2. Pavarde\n 3. Egzamino bala\n 4. Galutini bala, apskaiciuota su mediana\n 5. Galutini bala, apskaiciuota su vidurkiu\n Iveskite pasirinkimo numeri: ";
+	std::cout << std::endl << "Rusiuoti pagal:\n 1. Varda\n 2. Pavarde\n 3. Galutini bala, apskaiciuota su mediana\n 4. Galutini bala, apskaiciuota su vidurkiu\n Iveskite pasirinkimo numeri: ";
 	int Rusiavimo_Pasirinkimas;
 	std::cin >> Rusiavimo_Pasirinkimas;
-	// Sorting based on user's choice
-	switch (Rusiavimo_Pasirinkimas) {
-	case 1:
-		std::sort(S.begin(), S.end(), compareFirstName);
-		break;
-	case 2:
-		std::sort(S.begin(), S.end(), compareLastName);
-		break;
-	case 3:
-		std::sort(S.begin(), S.end(), compareExamGrade);
-		break;
-	case 4:
-		std::sort(S.begin(), S.end(), compareFinalGradeMedian);
-		break;
-	case 5:
-		std::sort(S.begin(), S.end(), compareFinalGradeAverage);
-		break;
-	default:
-		std::cout << "Netinkamas pasirinkimas\n";
+
+	while (Rusiavimo_Pasirinkimas != 1 && Rusiavimo_Pasirinkimas != 2 && Rusiavimo_Pasirinkimas != 3 && Rusiavimo_Pasirinkimas != 4 )
+	{
+		Netinkamas_Int(Rusiavimo_Pasirinkimas);
+	}
+	
+	switch (Rusiavimo_Pasirinkimas) 
+	{
+		case 1:
+			std::sort(S.begin(), S.end(), VarduRusiavimas);
+			break;
+		case 2:
+			std::sort(S.begin(), S.end(), PavardziuRusiavimas);
+			break;
+		case 3:
+			std::sort(S.begin(), S.end(), MedianuRusiavimas);
+			break;
+		case 4:
+			std::sort(S.begin(), S.end(), VidurkiuRusiavimas);
+			break;
 	}
 
 }
@@ -291,7 +261,7 @@ void Rusiuoti_Duomenis(std::vector<studentas>& S)
 void Spausdinti_Rezultatus(const std::vector<studentas>& S)
 {
 
-	std::cout << std::endl << "Kur norite spausdinti resultatus:\n 1. Ekrane \n 2. Faile \"Rezultatai.txt\"\n Iveskite pasirinkimo numeri: ";
+	std::cout << "\nKur norite spausdinti resultatus:\n 1. Ekrane \n 2. Faile \"Rezultatai.txt\"\n Iveskite pasirinkimo numeri: ";
 	int Spausdinimo_Pasirinkimas;
 	std::cin >> Spausdinimo_Pasirinkimas;
 	while (Spausdinimo_Pasirinkimas != 1 && Spausdinimo_Pasirinkimas != 2)
@@ -304,9 +274,9 @@ void Spausdinti_Rezultatus(const std::vector<studentas>& S)
 		for (int i = 0; i < S.size(); i++)
 		{
 			if (i == 0)
-				std::cout << std::setw(5) << "Nr." << std::setw(20) << "Pavarde" << std::setw(20) << "Vardas" << std::setw(20) << "Galutinis (Vid.)" << std::setw(20) << "Galutinis (Med.)" << std::endl << std::setfill('-') << std::setw(90) << "-" << std::setfill(' ') << std::endl;
+				std::cout << std::setw(5) << "Nr." << std::setw(20) << "Pavarde" << std::setw(20) << "Vardas" << std::setw(20) << "Galutinis (Vid.)" << std::setw(20) << "Galutinis (Med.)" << std::endl << std::setfill('-') << std::setw(90) << "-" << std::setfill(' ') << "\n";
 
-			std::cout << std::setw(5) << i + 1 << std::setw(20) << S[i].pavarde << std::setw(20) << S[i].vardas << std::setw(20) << std::setprecision(3) << S[i].GalutinisV << std::setw(20) << std::setprecision(3) << S[i].GalutinisM << std::endl;
+			std::cout << std::setw(5) << i + 1 << std::setw(20) << S[i].pavarde << std::setw(20) << S[i].vardas << std::setw(20) << std::setprecision(3) << S[i].GalutinisV << std::setw(20) << std::setprecision(3) << S[i].GalutinisM << "\n";
 
 
 		}
@@ -338,128 +308,151 @@ void Spausdinti_Rezultatus(const std::vector<studentas>& S)
 
 int main()
 {
-	std::cout << "Pasirinkite veiksma:\n 1. Suvesti visus studentu duomenis\n 2. Sugeneruoti tik studentu pazymius\n 3. Sugeneruoti studentu vardus ir pazymius\n 4. Nuskaityti studentu duomenis nuo failo\n 5. Baigti darba\n Iveskite pasirinkimo numeri: ";
 	int Pasirinkimas;
-	std::cin >> Pasirinkimas;
-	while (Pasirinkimas != 1 && Pasirinkimas != 2 && Pasirinkimas != 3 && Pasirinkimas != 4 && Pasirinkimas != 5)
+	do
 	{
-		Netinkamas_Int(Pasirinkimas);
-	}
-
-	if (Pasirinkimas == 1)
-	{
-		std::vector<studentas> S;
-		studentas naujas;
-		do
+		std::cout << "Pasirinkite veiksma:\n 1. Suvesti visus studentu duomenis\n 2. Sugeneruoti tik studentu pazymius\n 3. Sugeneruoti studentu vardus ir pazymius\n 4. Nuskaityti studentu duomenis nuo failo\n 5. Baigti darba\n Iveskite pasirinkimo numeri: ";
+		std::cin >> Pasirinkimas;
+		while (Pasirinkimas != 1 && Pasirinkimas != 2 && Pasirinkimas != 3 && Pasirinkimas != 4 && Pasirinkimas != 5)
 		{
-			Ivesti_Varda(naujas);
-
-			Ivesti_Pazymius(naujas);
-
-			std::cout << std::endl << "Iveskite egzamino pazymi: ";
-			std::cin >> naujas.EGZ;
-			while (std::cin.fail() || naujas.EGZ < 1 || naujas.EGZ > 10)
-			{
-				Netinkamas_Int(naujas.EGZ);
-			}
-
-			S.push_back(naujas);// pridedamas studentas i vektoriu
-
-			std::cout << std::endl << "Ar norite ivesti dar viena studenta? (T jei taip , N - ne): ";
-			std::cin >> TaipNe;
-			while (TaipNe != 'T' && TaipNe != 'N')
-			{
-				Netinkamas_Char(TaipNe);
-			}
-
-		} while (TaipNe == 'T');
-
-		Apskaiciuoti_Rezultatus(S);
-		Rusiuoti_Duomenis(S);
-		Spausdinti_Rezultatus(S);
-	}
-
-	if (Pasirinkimas == 2)
-	{
-		std::vector<studentas> S;
-		studentas naujas;
-		do
-		{
-			Ivesti_Varda(naujas);
-
-			GeneruotiNDPazymius(naujas);
-
-			naujas.EGZ = dis(generuoti);
-			std::cout << "Sugeneruotas egzamino balas: " << naujas.EGZ << std::endl;
-
-			std::cout << std::endl << std::endl << "Ar norite ivesti dar viena studenta? (T jei taip , N - ne): ";
-			std::cin >> TaipNe;
-			while (TaipNe != 'T' && TaipNe != 'N')
-			{
-				Netinkamas_Char(TaipNe);
-			}
-			S.push_back(naujas); // pridedamas studentas i vektoriu
-		} while (TaipNe == 'T');
-
-		Apskaiciuoti_Rezultatus(S);
-		Rusiuoti_Duomenis(S);
-		Spausdinti_Rezultatus(S);
-	}
-
-	if (Pasirinkimas == 3)
-	{
-		std::vector<studentas> S;
-		studentas naujas;
-		do
-		{
-			int lytis = dis_lytis(generuoti);
-
-			if (lytis == 0)
-				Generuoti_Varda_V(naujas);
-			else
-				Generuoti_Varda_M(naujas);
-
-			std::cout << std::endl << "Sugeneruotas vardas ir pavarde: " << naujas.vardas << " " << naujas.pavarde << std::endl;
-
-			GeneruotiNDPazymius(naujas);
-
-			naujas.EGZ = dis(generuoti);
-			std::cout << "Sugeneruotas egzamino balas: " << naujas.EGZ << std::endl;
-
-			std::cout << std::endl << std::endl << "Ar norite sugeneruoti dar viena studenta? (T jei taip , N - ne): ";
-			std::cin >> TaipNe;
-			while (TaipNe != 'T' && TaipNe != 'N')
-			{
-				Netinkamas_Char(TaipNe);
-			}
-			S.push_back(naujas); // pridedamas studentas i vektoriu
-		} while (TaipNe == 'T');
-
-		Apskaiciuoti_Rezultatus(S);
-		Rusiuoti_Duomenis(S);
-		Spausdinti_Rezultatus(S);
-	}
-
-	if (Pasirinkimas == 4)
-	{
-		std::vector<studentas> S;
-		std::cout << std::endl << "Pasirinkite, is kurio failo norite nuskaityti duomeniis:\n 1. \"kursiokai.txt\" \n 2. \"studentai10000.txt\"\n 3. \"studentai100000.txt\"\n 4. \"studentai1000000.txt\"\n Iveskite pasirinkimo numeri: ";
-		int Failo_Pasirinkimas;
-		std::cin >> Failo_Pasirinkimas;
-		while (Failo_Pasirinkimas != 1 && Failo_Pasirinkimas != 2 && Failo_Pasirinkimas != 3 && Failo_Pasirinkimas != 4)
-		{
-			Netinkamas_Int(Failo_Pasirinkimas);
+			Netinkamas_Int(Pasirinkimas);
 		}
-		if (Failo_Pasirinkimas == 1) S = Nuskaityti_Is_Failo("kursiokai.txt");
-		if (Failo_Pasirinkimas == 2) S = Nuskaityti_Is_Failo("studentai10000.txt");
-		if (Failo_Pasirinkimas == 3) S = Nuskaityti_Is_Failo("studentai100000.txt ");
-		if (Failo_Pasirinkimas == 4) S = Nuskaityti_Is_Failo("studentai1000000.txt ");
-		Apskaiciuoti_Rezultatus(S);
-		Rusiuoti_Duomenis(S);
-		Spausdinti_Rezultatus(S);
-	}
-	if (Pasirinkimas == 5)
-		std::cout << std::endl << "Darbas baigtas";
+
+		if (Pasirinkimas == 1)
+		{
+			std::vector<studentas> S;
+			studentas naujas;
+			do
+			{
+				Ivesti_Varda(naujas);
+
+				Ivesti_Pazymius(naujas);
+
+				std::cout << std::endl << "Iveskite egzamino pazymi: ";
+				std::cin >> naujas.EGZ;
+				while (std::cin.fail() || naujas.EGZ < 1 || naujas.EGZ > 10)
+				{
+					Netinkamas_Int(naujas.EGZ);
+				}
+
+				S.push_back(naujas);// pridedamas studentas i vektoriu
+				std::cout << std::endl << "Ar norite ivesti " << S.size() + 1 << " studenta ? (T jei taip, N - ne) : ";
+				std::cin >> TaipNe;
+				while (TaipNe != 'T' && TaipNe != 'N')
+				{
+					Netinkamas_Char(TaipNe);
+				}
+
+			} while (TaipNe == 'T');
+
+			Apskaiciuoti_Rezultatus(S);
+			Rusiuoti_Duomenis(S);
+			Spausdinti_Rezultatus(S);
+		}
+
+		if (Pasirinkimas == 2)
+		{
+			std::vector<studentas> S;
+			studentas naujas;
+			do
+			{
+				Ivesti_Varda(naujas);
+
+				std::cout << std::endl << "Kiek namu darbu pazymiu norite sugeneruoti: ";
+				int ND_kiekis;
+				std::cin >> ND_kiekis;
+
+				GeneruotiNDPazymius(naujas, ND_kiekis);
+
+				naujas.EGZ = dis(generuoti);
+
+				std::cout << std::endl << std::endl << "Ar norite ivesti dar viena studenta? (T jei taip , N - ne): ";
+				std::cin >> TaipNe;
+				while (TaipNe != 'T' && TaipNe != 'N')
+				{
+					Netinkamas_Char(TaipNe);
+				}
+				S.push_back(naujas); // pridedamas studentas i vektoriu
+			} while (TaipNe == 'T');
+
+			Apskaiciuoti_Rezultatus(S);
+			Rusiuoti_Duomenis(S);
+			Spausdinti_Rezultatus(S);
+		}
+
+		if (Pasirinkimas == 3)
+		{
+			std::vector<studentas> S;
+			studentas naujas;
+
+			std::cout << std::endl << "Kiek studentu norite sugeneruoti: ";
+			int Studentu_kiekis;
+			std::cin >> Studentu_kiekis;
+
+			std::cout << std::endl << "Kiek namu darbu pazymiu norite sugeneruoti: ";
+			int ND_kiekis;
+			std::cin >> ND_kiekis;
+
+			for (int i = 0; i < Studentu_kiekis; ++i)
+			{
+
+				int lytis = dis_lytis(generuoti);
+
+				if (lytis == 0)
+					Generuoti_Varda_V(naujas);
+				else
+					Generuoti_Varda_M(naujas);
+
+				GeneruotiNDPazymius(naujas, ND_kiekis);
+
+				naujas.EGZ = dis(generuoti);
+
+				S.push_back(naujas); // pridedamas studentas i vektoriu
+			}
+
+			Apskaiciuoti_Rezultatus(S);
+			Rusiuoti_Duomenis(S);
+			Spausdinti_Rezultatus(S);
+		}
+
+		if (Pasirinkimas == 4)
+		{
+			std::vector<studentas> S;
+			std::cout << "\nPasirinkite, is kurio failo norite nuskaityti duomeniis:\n 1. kursiokai.txt \n 2. studentai10000.txt\n 3. studentai100000.txt\n 4. studentai1000000.txt\n Iveskite pasirinkimo numeri: ";
+			int Failo_Pasirinkimas;
+			std::cin >> Failo_Pasirinkimas;
+			while (Failo_Pasirinkimas != 1 && Failo_Pasirinkimas != 2 && Failo_Pasirinkimas != 3 && Failo_Pasirinkimas != 4)
+			{
+				Netinkamas_Int(Failo_Pasirinkimas);
+			}
+			switch (Failo_Pasirinkimas)
+			{
+				case 1:
+				S = Nuskaityti_Is_Failo("kursiokai.txt");
+				break;
+
+				case 2:
+				S = Nuskaityti_Is_Failo("studentai10000.txt");
+				break;
+
+				case 3:
+				S = Nuskaityti_Is_Failo("studentai100000.txt ");
+				break;
+				case 4:
+				S = Nuskaityti_Is_Failo("studentai1000000.txt ");
+				break;
+			}
+	
+			Apskaiciuoti_Rezultatus(S);
+			Rusiuoti_Duomenis(S);
+			Spausdinti_Rezultatus(S);
+		}
+
+		if (Pasirinkimas == 5)
+			std::cout << "\n" << "Darbas baigtas";
+
+		std::cout << "\n";
+	}while (Pasirinkimas != 5);
 
 	return 0;
 }
