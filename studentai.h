fd9df2e5ff1a97949e09c296870b21628bc19002
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-02-28 21:34:28 +0200

    Add files via upload

---
+++ studentai.h
@@ -0,0 +1,18 @@
+#ifndef STUDENTAI_H
+#define STUDENTAI_H
+
+#include <string>
+#include <vector>
+
+struct studentas
+{
+    std::string vardas;
+    std::string pavarde;
+    std::vector<int> ND;
+    int EGZ;
+    double GalutinisV;
+    double GalutinisM;
+};
+
+
+#endif
