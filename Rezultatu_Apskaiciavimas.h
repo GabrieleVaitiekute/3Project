64e1664da518b6cd2ffae4f1a3655e831532c0ab
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-03-08 16:29:39 +0200

    Update Rezultatu_Apskaiciavimas.h

--- Rezultatu_Apskaiciavimas.h
+++ Rezultatu_Apskaiciavimas.h
@@ -1,7 +1,7 @@
 #ifndef REZULTATU_APSKAICIAVIMAS_H
 #define REZULTATU_APSKAICIAVIMAS_H
 #include "Studentai.h"
-void Apskaiciuoti_Rezultatus(std::vector<studentas>& S);
+void Apskaiciuoti_Rezultatus(std::vector<studentas>& S, std::vector<studentas>& N, std::vector<studentas>& G);
 
 #endif
 
