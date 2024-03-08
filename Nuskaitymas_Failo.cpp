8aaa56621543a41ba8637c6492fec02f08d491fb
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-03-08 16:36:33 +0200

    Update Nuskaitymas_Failo.cpp

--- Nuskaitymas_Failo.cpp
+++ Nuskaitymas_Failo.cpp
@@ -5,14 +5,20 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <windows.h>
 
-std::vector<studentas> Nuskaityti_Is_Failo(const std::string& Failo_Pavadinimas)
+
+std::vector<studentas> Nuskaityti_Is_Failo(const std::string& Failo_Pavadinimas, int reserveDydis)
 {
 	// Pradedamas skaiciuti laikas
 	auto start = std::chrono::high_resolution_clock::now();
 
+	// Open the file in binary mode
 	std::ifstream file(Failo_Pavadinimas);
+
+	
 	std::vector<studentas> S;
+	S.reserve( reserveDydis);
 
 	if (!file.is_open())
 	{
@@ -50,13 +56,15 @@ std::vector<studentas> Nuskaityti_Is_Failo(const std::string& Failo_Pavadinimas)
 
 		S.push_back(student); // Pridedamas studentas 
 	}
+
+	
 	// Baigia skaiciuoti laika
 	auto end = std::chrono::high_resolution_clock::now();
 
 	//Apskaiciuoja laika
 	auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
 
-	std::cout << "Failo nuskaitymas uztruko " << duration.count() << " sekundes(e)" << std::endl;
+	std::cout << "\nFailo nuskaitymas uztruko " << duration.count() << " sekundes(e)" << std::endl;
 	file.close();
 	return S;
 }
