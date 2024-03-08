b45e717107694c8138603ebba6af383380efd41f
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-03-08 23:44:58 +0200

    Update Nuskaitymas_Failo.h

--- Nuskaitymas_Failo.h
+++ Nuskaitymas_Failo.h
@@ -2,10 +2,6 @@
 #define NUSKAITYMAS_FAILO_H
 
 #include "Studentai.h"
-#include <vector>
-#include <iostream>
-#include <fstream>
-#include <sstream>
 
 std::vector<studentas> Nuskaityti_Is_Failo(const std::string& Failo_Pavadinimas, int reserveDydis);
 
