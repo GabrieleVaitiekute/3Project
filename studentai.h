0dc0b1ffbe96dc1e325c579712629a27d88c30f2
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-03-08 16:28:26 +0200

    Update studentai.h

--- studentai.h
+++ studentai.h
@@ -1,8 +1,16 @@
 #ifndef STUDENTAI_H
 #define STUDENTAI_H
-
+#include "Netinkama_Ivestis.h"
 #include <string>
 #include <vector>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <stdexcept>
+#include <limits>
+#include <filesystem>
+
 
 struct studentas
 {
