ce5851fe8590c69ee789183c8283ba34593942c2
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-03-08 16:30:33 +0200

    Update Generavimas.h

--- Generavimas.h
+++ Generavimas.h
@@ -3,10 +3,10 @@
 
 #include "Studentai.h"
 #include <random>
+#include <chrono>
 void GeneruotiNDPazymius(studentas& S, int ND_kiekis);
 
 void GeneruotiVardus(studentas& S);
 
 void GeneruotiFailus(int reserveDydis, std::string failoPav);
-#endif // !GENERAVIMAS_H
-
+#endif 
