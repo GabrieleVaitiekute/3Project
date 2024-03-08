482270fffa2b1ffa7bbc4adce4b9972894a84700
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-03-08 23:45:11 +0200

    Update Rezultatu_Apskaiciavimas.cpp

--- Rezultatu_Apskaiciavimas.cpp
+++ Rezultatu_Apskaiciavimas.cpp
@@ -1,11 +1,9 @@
 #include "Rezultatu_Apskaiciavimas.h"
 #include "Studentai.h"
-#include <numeric>
-#include <algorithm>
 
 void Apskaiciuoti_Rezultatus(std::vector<studentas>& S, std::vector<studentas>& N, std::vector<studentas>& G)
 {
-	std::cout << "Ar norite studentus surusiuoti pagal mediana ar vidurki? M jei mediana, V jei vidurki: ";
+	std::cout << "\nAr norite studentus surusiuoti pagal mediana ar vidurki? M jei mediana, V jei vidurki: ";
 	char RusiavimoPasirinkimas;
 	while (true)
 	{
@@ -23,6 +21,9 @@ void Apskaiciuoti_Rezultatus(std::vector<studentas>& S, std::vector<studentas>& 
 			Netinkamas_Ivestis(rp.what());
 		}
 	}
+	// Pradedamas skaiciuti laikas
+	auto RusavimoPradzia = std::chrono::high_resolution_clock::now();
+
 	for (int i = 0; i < S.size(); i++)
 	{
 		//apskaiciuojama su vidurkiu
@@ -53,7 +54,8 @@ void Apskaiciuoti_Rezultatus(std::vector<studentas>& S, std::vector<studentas>& 
 				S[i].GalutinisM = 0.4 * mediana + 0.6 * S[i].EGZ;
 			}
 		}
-		
+
+
 		if (RusiavimoPasirinkimas == 'V')
 		{
 
@@ -78,5 +80,14 @@ void Apskaiciuoti_Rezultatus(std::vector<studentas>& S, std::vector<studentas>& 
 				G.push_back(S[i]);
 			}
 		}
+
 	}
+
+	// Baigia skaiciuoti laika
+	auto RusaivimoPabaiga = std::chrono::high_resolution_clock::now();
+
+	//Apskaiciuoja laika
+	auto Rusiavimotrukme = std::chrono::duration_cast<std::chrono::seconds>(RusaivimoPabaiga - RusavimoPradzia);
+
+	std::cout << "\nRusiavimas i galvocius ir nepazangius uztruko " << Rusiavimotrukme.count() << " sek." << std::endl;
 }
