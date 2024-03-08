618d0037666a2a32ffa15e999a214a5ae3e2c28a
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-03-08 16:28:44 +0200

    Update Rezultatu_Spausdinimas.h

--- Rezultatu_Spausdinimas.h
+++ Rezultatu_Spausdinimas.h
@@ -2,7 +2,7 @@
 #define REZULTATU_SPAUSDINIMAS_H
 #include "Studentai.h"
 
-void Spausdinti_Rezultatus(const std::vector<studentas>& S);
+void Spausdinti_Rezultatus(const std::vector<studentas>& N, const std::vector<studentas>& G);
 
 #endif
 
