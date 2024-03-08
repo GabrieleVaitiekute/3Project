0cd47e30fb109036c9b1e2ab708a36d650818642
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-03-08 23:43:48 +0200

    Update Netinkama_Ivestis.cpp

--- Netinkama_Ivestis.cpp
+++ Netinkama_Ivestis.cpp
@@ -1,11 +1,7 @@
 #include "Netinkama_Ivestis.h"
 #include "Studentai.h"
-#include <iostream>
-#include <fstream>
-#include <sstream>
 
-
-void Netinkamas_Ivestis( std::string Problema)
+void Netinkamas_Ivestis(std::string Problema)
 {
 	std::cin.clear();
 	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
