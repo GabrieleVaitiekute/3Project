ec682c8c8cfd2e3e7a79c0c6ee0cbd90a1b8ffcc
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-03-22 14:49:50 +0200

    Delete Netinkama_Ivestis.cpp

--- Netinkama_Ivestis.cpp
+++
@@ -1,9 +0,0 @@
-#include "Netinkama_Ivestis.h"
-#include "Studentai.h"
-
-void Netinkamas_Ivestis(std::string Problema)
-{
-	std::cin.clear();
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	std::cerr << Problema;
-}
