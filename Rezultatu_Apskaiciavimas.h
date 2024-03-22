47780a692af01e624a91e03db82bf6f96e46736e
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-03-22 14:50:28 +0200

    Delete Rezultatu_Apskaiciavimas.h

--- Rezultatu_Apskaiciavimas.h
+++
@@ -1,8 +0,0 @@
-#ifndef REZULTATU_APSKAICIAVIMAS_H
-#define REZULTATU_APSKAICIAVIMAS_H
-#include "Studentai.h"
-void Apskaiciuoti_Rezultatus(std::vector<studentas>& S, std::vector<studentas>& N, std::vector<studentas>& G);
-
-#endif
-
-
