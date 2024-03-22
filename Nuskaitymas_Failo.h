f0ee3817573f4d06daf361b7b3e2cf1759a7ccc2
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-03-22 14:50:13 +0200

    Delete Nuskaitymas_Failo.h

--- Nuskaitymas_Failo.h
+++
@@ -1,8 +0,0 @@
-#ifndef NUSKAITYMAS_FAILO_H
-#define NUSKAITYMAS_FAILO_H
-
-#include "Studentai.h"
-
-std::vector<studentas> Nuskaityti_Is_Failo(const std::string& Failo_Pavadinimas, int reserveDydis);
-
-#endif
