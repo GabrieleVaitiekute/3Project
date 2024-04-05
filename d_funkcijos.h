983a7d8e1a447c10afbb8b90840097638c590c20
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-04-05 15:21:06 +0300

    Update d_funkcijos.h

--- d_funkcijos.h
+++ d_funkcijos.h
@@ -5,13 +5,6 @@
 //// NETINKAMA IVESTIS
 void Netinkamas_Ivestis(std::string Problema);
 
-//// DUOMENU RUSIAVIMAS
-bool VarduRusiavimas(const studentas& a, const studentas& b);
-bool PavardziuRusiavimas(const studentas& a, const studentas& b);
-bool MedianuRusiavimas(const studentas& a, const studentas& b);
-bool VidurkiuRusiavimas(const studentas& a, const studentas& b);
-void Rusiuoti_Duomenis(std::deque<studentas>& N, std::deque<studentas>& G);
-
 //// DUOMENU GENERAVIMAS
 void GeneruotiNDPazymius(studentas& S, int ND_kiekis);
 void GeneruotiVardus(studentas& S);
@@ -21,13 +14,22 @@ void GeneruotiFailus(int reserveDydis, std::string& failoPav);
 void Ivesti_Pazymius(studentas& S);
 void Ivesti_Varda(studentas& S);
 
-
 //// NUSKAITYMAS NUO FAILO
 std::deque<studentas> Nuskaityti_Is_Failo(const std::string& Failo_Pavadinimas, int reserveDydis);
 
-//// REZULTATU APSKAICIAVIMAS IR SKIRSTYMAS I GRUPES 
-void Apskaiciuoti_Rezultatus(std::deque<studentas>& S, std::deque<studentas>& N, std::deque<studentas>& G);
+//// REZULTATU APSKAICIAVIMAS 
+void Apskaiciuoti_Rezultatus(std::deque<studentas>& S);
+
+//// DUOMENU RUSIAVIMAS
+bool VarduRikiavimas(const studentas& a, const studentas& b);
+bool PavardziuRikiavimas(const studentas& a, const studentas& b);
+bool MedianuRikiavimas(const studentas& a, const studentas& b);
+bool VidurkiuRikiavimas(const studentas& a, const studentas& b);
+void Rikiuoti_Duomenis(std::deque<studentas>& S);
+
+//// STUDENTU SKIRSTYMAS
+void Skirstyti_Studentus(std::deque<studentas>& S, std::deque<studentas>& N, std::deque<studentas>& G);
 
 //// SPAUSDINIMAS
-void Spausdinti_Rezultatus(const std::deque<studentas>& N, const std::deque<studentas>& G);
+void Spausdinti_Rezultatus(const std::deque<studentas>& S, const std::deque<studentas>& N, const std::deque<studentas>& G);
 #endif 
