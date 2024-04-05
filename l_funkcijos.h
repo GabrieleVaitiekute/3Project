952c4ab92808e5913ae5bd86a66a589ffd606daa
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-04-05 15:25:32 +0300

    Update l_funkcijos.h

--- l_funkcijos.h
+++ l_funkcijos.h
@@ -4,13 +4,6 @@
 ////// NETINKAMA IVESTIS
 void Netinkamas_Ivestis(std::string Problema);
 
-//// DUOMENU RUSIAVIMAS
-bool VarduRusiavimas(const studentas& a, const studentas& b);
-bool PavardziuRusiavimas(const studentas& a, const studentas& b);
-bool MedianuRusiavimas(const studentas& a, const studentas& b);
-bool VidurkiuRusiavimas(const studentas& a, const studentas& b);
-void Rusiuoti_Duomenis(std::list<studentas>& N, std::list<studentas>& G);
-
 //// GENERAVIMAS
 void GeneruotiNDPazymius(studentas& S, int ND_kiekis);
 void GeneruotiVardus(studentas& S);
@@ -23,10 +16,21 @@ void Ivesti_Varda(studentas& S);
 //// NUSKAITYMAS NUO FAILO
 std::list<studentas> Nuskaityti_Is_Failo(const std::string& Failo_Pavadinimas, int reserveDydis);
 
-//// REZULTATU APSKAICIAVIMAS IRSKIRSTYMAS I GRUPES
-void Apskaiciuoti_Rezultatus(std::list<studentas>& S, std::list<studentas>& N, std::list<studentas>& G);
+//// REZULTATU APSKAICIAVIMAS
+void Apskaiciuoti_Rezultatus(std::list<studentas>& S);
+
+//// DUOMENU RUSIAVIMAS
+bool VarduRikiavmas(const studentas& a, const studentas& b);
+bool PavardziuRikiavmas(const studentas& a, const studentas& b);
+bool MedianuRikiavmas(const studentas& a, const studentas& b);
+bool VidurkiuRikiavmas(const studentas& a, const studentas& b);
+void Rikiuoti_Duomenis(std::list<studentas>& S);
+
+//// STUDENTU SKIRSTYMAS
+void Skirstyti_Studentus(std::list<studentas>& S, std::list<studentas>& N, std::list<studentas>& G);
 
 //// REZULTATU SPAUSDINIMAS
 void Spausdinti_Rezultatus(const std::list<studentas>& N, const std::list<studentas>& G);
-#endif 
 
+
+#endif 
