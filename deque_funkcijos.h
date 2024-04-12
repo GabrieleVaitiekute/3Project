1cc31640647a7931acfd90317ab84789d9757c98
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-04-12 18:38:41 +0300

    Add files via upload

---
+++ deque_funkcijos.h
@@ -0,0 +1,38 @@
+#ifndef DEQUE_FUNKCIJOS_H
+#define DEQUE_FUNKCIJOS_H
+#include "deque_studentai.h"
+
+//// NETINKAMA IVESTIS
+void Netinkamas_Ivestis(std::string Problema);
+
+
+
+//// DUOMENU GENERAVIMAS
+void GeneruotiNDPazymius(studentas& S, int ND_kiekis);
+void GeneruotiVardus(studentas& S);
+void GeneruotiFailus(int reserveDydis, std::string& failoPav);
+
+//// DUOMENU IVEDIMAS
+void Ivesti_Pazymius(studentas& S);
+void Ivesti_Varda(studentas& S);
+
+
+//// NUSKAITYMAS NUO FAILO
+std::deque<studentas> Nuskaityti_Is_Failo(const std::string& Failo_Pavadinimas, int reserveDydis);
+
+//// REZULTATU APSKAICIAVIMAS 
+void Apskaiciuoti_Rezultatus(std::deque<studentas>& S);
+
+//// DUOMENU RUSIAVIMAS
+bool VarduRikiavimas(const studentas& a, const studentas& b);
+bool PavardziuRikiavimas(const studentas& a, const studentas& b);
+bool MedianuRikiavimas(const studentas& a, const studentas& b);
+bool VidurkiuRikiavimas(const studentas& a, const studentas& b);
+void Rikiuoti_Duomenis(std::deque<studentas>& S);
+
+//// STUDENTU SKIRSTYMAS
+void Skirstyti_Studentus(std::deque<studentas>& S, std::deque<studentas>& N, std::deque<studentas>& G, int Strategija);
+
+//// SPAUSDINIMAS
+void Spausdinti_Rezultatus(const std::deque<studentas>& N, const std::deque<studentas>& G);
+#endif 
