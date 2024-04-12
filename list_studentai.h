a0006eaa73e4d324bee67af23bf117d25f84d3dc
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-04-12 18:15:54 +0300

    Add files via upload

---
+++ list_studentai.h
@@ -0,0 +1,29 @@
+#ifndef LIST_STUDENTAI_H
+#define LIST_STUDENTAI_H
+
+#include <chrono> 
+#include <string>
+#include <list>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <stdexcept>
+#include <limits>
+#include <filesystem>
+#include <algorithm>
+#include <numeric> 
+#include <random> 
+
+
+struct studentas
+{
+    std::string vardas;
+    std::string pavarde;
+    std::list<int> ND;
+    int EGZ = 0;
+    double GalutinisV = 0;
+    double GalutinisM = 0;
+
+};
+#endif
+
