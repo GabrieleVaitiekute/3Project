0a0cf853cbd896ddc7fdbc11513e1a5301288cbc
GabrieleVaitiekute <147078486+GabrieleVaitiekute@users.noreply.github.com>
2024-04-19 22:30:56 +0300

    Update class_studentai.h

--- class_studentai.h
+++ class_studentai.h
@@ -5,7 +5,6 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <string>
 #include <sstream>
 #include <fstream>
 #include <stdexcept>
@@ -14,56 +13,143 @@
 #include <algorithm>
 #include <numeric> 
 #include <random> 
+#include <cassert>
 
 class studentas {
 private:
-	std::string vardas;
-	std::string pavarde;
-	std::vector<int> ND;
-	int EGZ = 0;
-	double GalutinisV = 0;
-	double GalutinisM = 0;
-
-	void calculateFinalScores()
-	{
-		GalutinisV = 0.4 * std::accumulate(ND.begin(), ND.end(), 0.0) / ND.size() + 0.6 * EGZ;
-		if (ND.size() > 1) {
-			std::vector<int> sortedND = ND;
-			std::sort(sortedND.begin(), sortedND.end());
-			size_t mid = sortedND.size() / 2;
-			GalutinisM = 0.4 * (sortedND.size() % 2 == 0 ? (sortedND[mid - 1] + sortedND[mid]) / 2.0 : sortedND[mid]) + 0.6 * EGZ;
-		}
-		else if (!ND.empty()) {
-			GalutinisM = 0.4 * ND[0] + 0.6 * EGZ;
-		}
-	}
+    std::string vardas;
+    std::string pavarde;
+    std::vector<int> ND;
+    int EGZ = 0;
+    double GalutinisV = 0;
+    double GalutinisM = 0;
+    void studentas::ApskaiciuotiGalutinius()
+    {
+        if (!ND.empty())
+        {
+            GalutinisV = 0.4 * std::accumulate(ND.begin(), ND.end(), 0.0) / ND.size() + 0.6 * EGZ;
+            if (ND.size() > 1)
+            {
+                std::vector<int> sortedND = ND;
+                std::sort(sortedND.begin(), sortedND.end());
+                size_t mid = sortedND.size() / 2;
+                GalutinisM = 0.4 * (sortedND.size() % 2 == 0 ? (sortedND[mid - 1] + sortedND[mid]) / 2.0 : sortedND[mid]) + 0.6 * EGZ;
+            }
+            else
+                GalutinisM = 0.4 * ND[0] + 0.6 * EGZ;
+        }
+        else
+        {
+            // Jei ND tuščias
+            GalutinisV = GalutinisM = 0.6 * EGZ;
+        }
+    }
 
 public:
-	// Constructors
-	studentas() = default;
-	studentas(const std::string& vardas, const std::string& pavarde, const std::vector<int>& ND, int EGZ)
-		: vardas(vardas), pavarde(pavarde), ND(ND), EGZ(EGZ) {
-		calculateFinalScores();
-	}
+    studentas::studentas() = default;
 
-	// Destructor
-	~studentas() { }
+    studentas::studentas(const std::string& vardas, const std::string& pavarde, const std::vector<int>& ND, int EGZ)
+        : vardas(vardas), pavarde(pavarde), ND(ND), EGZ(EGZ) {
+        ApskaiciuotiGalutinius(); // Calculate final scores after initialization
+    }
 
-	// Getters
-	std::string getVardas() const { return vardas; }
-	std::string getPavarde() const { return pavarde; }
-	std::vector<int> getND() const { return ND; }
-	int getEGZ() const { return EGZ; }
-	double getGalutinisV() const { return GalutinisV; }
-	double getGalutinisM() const { return GalutinisM; }
+    // Destruktorius
+    studentas::~studentas() = default;
 
-	// Setters
-	void setVardas(const std::string& newName) { vardas = newName; }
-	void setPavarde(const std::string& newSurname) { pavarde = newSurname; }
-	void setND(const std::vector<int>& newND) { ND = newND; }
-	void setEGZ(int newEGZ) {
-		EGZ = newEGZ;
-		calculateFinalScores();
-	}
-};
+    // Copy konstruktorius
+    studentas::studentas(const studentas& other)
+        : vardas(other.vardas), pavarde(other.pavarde), ND(other.ND), EGZ(other.EGZ),
+        GalutinisV(other.GalutinisV), GalutinisM(other.GalutinisM) {}
+
+    // Move konstruktorius
+    studentas::studentas(studentas&& other) noexcept
+        : vardas(std::move(other.vardas)), pavarde(std::move(other.pavarde)), ND(std::move(other.ND)),
+        EGZ(other.EGZ), GalutinisV(other.GalutinisV), GalutinisM(other.GalutinisM) {}
+
+    // Copy priskyrimo operatorius
+    studentas& studentas::operator=(const studentas& other) {
+        if (this != &other) {
+            vardas = other.vardas;
+            pavarde = other.pavarde;
+            ND = other.ND;
+            EGZ = other.EGZ;
+            GalutinisV = other.GalutinisV;
+            GalutinisM = other.GalutinisM;
+        }
+        return *this;
+    }
+
+    // Move priskyrimo operatorius
+    studentas& studentas::operator=(studentas&& other) noexcept {
+        if (this != &other) {
+            vardas = std::move(other.vardas);
+            pavarde = std::move(other.pavarde);
+            ND = std::move(other.ND);
+            EGZ = other.EGZ;
+            GalutinisV = other.GalutinisV;
+            GalutinisM = other.GalutinisM;
+        }
+        return *this;
+    }
+
+    // Getter'iai
+    std::string studentas::getVardas() const { return vardas; }
+    std::string studentas::getPavarde() const { return pavarde; }
+    std::vector<int> studentas::getND() const { return ND; }
+    int studentas::getEGZ() const { return EGZ; }
+    double studentas::getGalutinisV() const { return GalutinisV; }
+    double studentas::getGalutinisM() const { return GalutinisM; }
+
+    // Setter'iai
+    void studentas::setVardas(const std::string& newName) { vardas = newName; }
+    void studentas::setPavarde(const std::string& newSurname) { pavarde = newSurname; }
+    void studentas::setND(const std::vector<int>& newND) { ND = newND; ApskaiciuotiGalutinius(); }
+    void studentas::setEGZ(int newEGZ) {
+        EGZ = newEGZ;
+        ApskaiciuotiGalutinius();
+    }
+
+    friend std::istream& operator>>(std::istream& is, studentas& s)
+    {
+        s.vardas.clear();
+        s.pavarde.clear();
+        s.ND.clear();
+        s.EGZ = 0;  
+
+        
+        if (!(is >> s.vardas >> s.pavarde)) {
+            return is;  
+        }
+
+        int pazymys;
+        std::vector<int> grades;
+        while (is >> pazymys) {
+            grades.push_back(pazymys);
+        }
+
+        // Patikrina, ar pasiekė failo pabaigą
+        if (is.eof()) {
+            is.clear();
+        }
+        // Jei įvedimo operacija nepavyko
+        else if (is.fail()) {
+   
+            is.clear();
+            std::string unused;
+            std::getline(is, unused); 
+            return is;
+        }
+
+        if (!grades.empty()) {
+            s.EGZ = grades.back(); 
+            grades.pop_back();
+            s.ND = grades;
+        }
+
+        s.ApskaiciuotiGalutinius();  
+
+        return is;
+    }
+    };
+
 #endif
